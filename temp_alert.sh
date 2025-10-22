775cba9e9d98233a690727ca1e5fdff2906191c8
saidarshan74 <141834083+saidarshan7@users.noreply.github.com>
2025-10-22 18:21:30 +0530

    added battery checking functionality

--- temp_alert.sh
+++ temp_alert.sh
@@ -10,24 +10,22 @@ python3 parse.py
 
 #to decide whether a tempreture is more than 70
 
+bat=$( acpi | grep -E -o '[0-9]{2}%' ) 
+b_p=$(echo $bat | sed 's/%//g')
+echo $b_p
+
+
 while read line; do
 
     num=$(($line+0))
     
-    if [ $num -gt 60 ];then
-        echo "Alert : System is getting HOT 🔥"
+    if [ $num -gt 70  ] || [ $b_p -gt 80 ];then
+        echo "Alert : Remove Charger $b_p % "
         echo "Tempreture reached : $num °C"
+	notify-send "Alert : 🔌 Remove Charger $b_p % " "System Tempreture : $num °C"
         break
     fi
 done < data.txt
-
-
-
-
-
-
- 
-#sleep 1 
 
 
 
