67313b50df3aed8668a7740b75a7756aff7a8fe7
saidarshan74 <141834083+saidarshan7@users.noreply.github.com>
2025-10-22 15:45:50 +0530

    main bash script

--- temp_alert.sh
+++ temp_alert.sh
@@ -1,9 +1,33 @@
 ##!/bin/sh
 
-# Print something, then run ls
 
-data=$( sensors -j)  #stores sensors data in json format
+data=$( sensors -j) #stores sensors data in json format
+
+echo "" > data.txt #empty file before inserting data
+
+echo $data > data.txt 
+python3 parse.py
+
+#to decide whether a tempreture is more than 70
+
+while read line; do
+
+    num=$(($line+0))
+    
+    if [ $num -gt 60 ];then
+        echo "Alert : System is getting HOT 🔥"
+        echo "Tempreture reached : $num °C"
+        break
+    fi
+done < data.txt
+
+
+
+
+
+
+ 
 #sleep 1 
 
-echo $data;
+
 
